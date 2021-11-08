25bf8900e3c0da7f0dcec8cea8cc7aff9daac84d
Cemil99 <teostore99@gmail.com>
2021-11-08 11:28:08 +0300

    before day 25 paralel testing

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         strict = true,
         plugin = {"json:target/cucumber.json",          // our report file gets the test data from cucumber.json file
+// json: xml file, keeps data in key and value structure, this plugin creates target / cucumber.json file
                 "html:target/default-html-reports", // default cucumber html report
-                "rerun:target/rerun.txt"},    // json: xml file, keeps data in key and value structure
-                                                         // this plugin creates target / cucumber.json file
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
      // dryRun = true,
--- src/test/resources/features/AccountTypes.feature
+++ src/test/resources/features/AccountTypes.feature
@@ -5,17 +5,17 @@ Feature: Account Types
     When the user navigates to "Activities" "Calendar Events"
     Then the title contains "Calendar Events - Activities"
 
-
+@wip
   Scenario: Store manager user
     Given the user logged in as "store manager"
     When the user navigates to "Customers" "Contacts"
     Then the title contains "Contacts - Customers"
 
-
+@wip
   Scenario: Sales manager user
     Given the user logged in as "sales manager"
     When the user navigates to "Customers" "Accounts"
-    Then the title contains "Accounts - Customers"
+    Then the title contains "Accounts - CustomersS"
 
 
 
